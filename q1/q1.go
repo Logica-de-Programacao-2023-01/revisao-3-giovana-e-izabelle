fa8c4042392d2b45b911d9298056511db85de1c5
iza <126572391+izabellealencar05@users.noreply.github.com>
2023-06-16 15:05:00 -0300

    Update q1.go

--- q1/q1.go
+++ q1/q1.go
@@ -1,21 +1,14 @@
 package q1
 
-//Dado um array de números inteiros "nums" e um número inteiro "target", retorne os índices dos dois números cuja soma
-//seja igual a "target".
-//
-//Você pode assumir que cada entrada terá exatamente uma solução e não poderá usar o mesmo elemento duas vezes.
-//
-//Você pode retornar a resposta em qualquer ordem.
-
-func TwoSum(nums []int, target int) []int {
+func TwoSum(nums []int, targe int) []int{
 	Map := make(map[int]int)
 
-	for i, num := range nums {
+	for i, num := range nums{
 		complemento := target - num
-		if index, ok := Map[complemento]; ok {
+		if index, ok := Map[complemento]; ok{
 			return []int{index, i}
 		}
 		Map[num] = i
 	}
-	return []int{}
-}
+	return nil
+}	
